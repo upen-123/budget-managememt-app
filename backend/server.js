9e82682d6626325510ae14c1c7c93fb3d128ab97
Upendra Rajpoot <upendrarajpoot@Upendras-MacBook-Air.local>
2024-07-19 17:13:18 +0530

    server file has been updated

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
-const port = 5000;
+const port = 80;
 
 app.use(express.json());
 app.use(cors());
